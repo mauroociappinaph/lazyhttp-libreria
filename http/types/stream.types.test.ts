5e907fafe7bbacc937593ee3510690aa0f802473
Tu Nombre <usuario@ejemplo.com>
2025-07-14 15:37:27 -0300

    feat(auth): agrega tipos de autenticación y pruebas para asegurar la correcta exportación de los mismos

---
+++ http/types/stream.types.test.ts
@@ -0,0 +1,7 @@
+import * as StreamTypes from './stream.types';
+
+describe('StreamTypes', () => {
+  it('should export all stream types', () => {
+    expect(typeof {} as StreamTypes.StreamConfig).toBeDefined();
+  });
+});
